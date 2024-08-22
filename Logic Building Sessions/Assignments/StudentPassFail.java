3c85877a94cb26c3d49c2e10a22966f21a53efbf
owalrohit <86915070+owalrohit@users.noreply.github.com>
2024-08-22 15:50:22 +0530

    Assignment 2 coding que

--- Logic Building Sessions/Assignments/StudentPassFail.java
+++ Logic Building Sessions/Assignments/StudentPassFail.java
@@ -7,12 +7,29 @@ more subjects, print the number of subjects they failed in.
 class StudentPassFail{
 	public static void main(String args[]){
 		int sub1 = 45;
-		int sub2 = 42;
+		int sub2 = 22;
 		int sub3 = 44;
+		int count =0;
 		
-		if(sub1>40 && sub2>40 &&sub>40){
-			System.out.println("passed in all subjects. ");
+		if(sub1<40){
+			count++;
+		}else if(sub2<40){
+			count++;
+		}else if(sub3<40){
+			count++;
+		}else{
+			System.out.println("Student is passed in all 3 subjects.");
 		}
+		
+		switch(count){
+			case 1:System.out.println("Student failed in 1 subject.");
+			break;
+			case 2:System.out.println("Student failed in 2 subjects.");
+			break;
+			case 3:System.out.println("Student failed in 3 subjects.");
+			break;
+		}
+		
 		
 		
 		
